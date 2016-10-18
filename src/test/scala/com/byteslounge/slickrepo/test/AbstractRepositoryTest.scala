366a506b6e85145caed73e680e9a07d9bad15f2b
Gonçalo Marques <goncalodinismarques@gmail.com>
2016-10-18 23:39:58 +0100

    Wait for test databases to initialize

--- src/test/scala/com/byteslounge/slickrepo/test/AbstractRepositoryTest.scala
+++ src/test/scala/com/byteslounge/slickrepo/test/AbstractRepositoryTest.scala
@@ -2,11 +2,14 @@ package com.byteslounge.slickrepo.test
 
 import com.byteslounge.slickrepo.repository.{CarRepository, CoffeeRepository, PersonRepository, TestIntegerVersionedEntityRepository}
 import org.scalatest.{BeforeAndAfter, FlatSpec, Matchers}
+import org.slf4j.LoggerFactory
 
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
 
 abstract class AbstractRepositoryTest(val config: Config) extends FlatSpec with BeforeAndAfter with Matchers {
+
+  val logger = LoggerFactory.getLogger(classOf[AbstractRepositoryTest])
 
   val driver = config.driver
   import driver.api._
@@ -24,6 +27,7 @@ abstract class AbstractRepositoryTest(val config: Config) extends FlatSpec with 
 
   before {
     initializeDb()
+    waitInitialized()
     createSchema()
   }
 
@@ -41,24 +45,34 @@ abstract class AbstractRepositoryTest(val config: Config) extends FlatSpec with 
   }
 
   def createSchema() {
-    val max: Int = 10
-    var attempts:Int = 1
-    while (attempts <= max) {
-      try {
-        executeAction(DBIO.seq(personRepository.tableQuery.schema.create, carRepository.tableQuery.schema.create, coffeeRepository.tableQuery.schema.create, testIntegerVersionedEntityRepository.tableQuery.schema.create))
-        attempts = 11
-      } catch {
-        case e: Exception =>
-      }
-      attempts = attempts + 1
-      if(attempts <= max){
-        Thread.sleep(120000)
-      }
-    }
+    executeAction(DBIO.seq(personRepository.tableQuery.schema.create, carRepository.tableQuery.schema.create, coffeeRepository.tableQuery.schema.create, testIntegerVersionedEntityRepository.tableQuery.schema.create))
   }
 
   def dropSchema() {
     executeAction(DBIO.seq(coffeeRepository.tableQuery.schema.drop, carRepository.tableQuery.schema.drop, personRepository.tableQuery.schema.drop, testIntegerVersionedEntityRepository.tableQuery.schema.drop))
   }
 
+  def waitInitialized() = {
+    val attempts = 40
+    val sleep = 30000L
+    var initialized = false
+    (1 to attempts).foreach(
+      i =>
+        try {
+          if(!initialized){
+            var query = config.validationQuery
+            executeAction(sql"#$query".as[(Int)].headOption)
+            logger.info("Connected to database " + config.dbConfig)
+            initialized = true
+          }
+        } catch {
+          case e: Exception =>
+            val message = if(i < attempts) "Will wait " + sleep + " ms before retry" else "Giving up"
+            logger.warn("Could not connect to database " + config.dbConfig + " [attempt " + i + "]. " + message)
+            if(i < attempts){
+              Thread.sleep(sleep)
+            }
+        }
+    )
+  }
 }
--- src/test/scala/com/byteslounge/slickrepo/test/DatabaseConfig.scala
+++ src/test/scala/com/byteslounge/slickrepo/test/DatabaseConfig.scala
@@ -4,24 +4,24 @@ import com.typesafe.slick.driver.db2.DB2Driver
 import com.typesafe.slick.driver.oracle.OracleDriver
 import slick.driver.{H2Driver, JdbcProfile, MySQLDriver}
 
-case class Config(driver: JdbcProfile, dbConfig: String, rollbackTxError: Int, rowLockTimeoutError: Int)
+case class Config(driver: JdbcProfile, dbConfig: String, rollbackTxError: Int, rowLockTimeoutError: Int, validationQuery: String)
 
 abstract class DatabaseConfig {
   def config: Config
 }
 
 object H2Config extends DatabaseConfig {
-  override def config: Config = Config(H2Driver, "h2", 23505, 50200)
+  override def config: Config = Config(H2Driver, "h2", 23505, 50200, "select 1")
 }
 
 object MySQLConfig extends DatabaseConfig {
-  override def config: Config = Config(MySQLDriver, "mysql", 1062, 1213)
+  override def config: Config = Config(MySQLDriver, "mysql", 1062, 1213, "select 1")
 }
 
 object OracleConfig extends DatabaseConfig {
-  override def config: Config = Config(OracleDriver, "oracle", 1, 60)
+  override def config: Config = Config(OracleDriver, "oracle", 1, 60, "select 1 from dual")
 }
 
 object DB2Config extends DatabaseConfig {
-  override def config: Config = Config(DB2Driver, "db2", 1, 60)
+  override def config: Config = Config(DB2Driver, "db2", 1, 60, "select 1 from sysibm.sysdummy1")
 }
