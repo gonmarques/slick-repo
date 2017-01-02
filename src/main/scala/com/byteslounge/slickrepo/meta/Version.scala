e0f0be6bebb4bd8acbd1eb701a031f4b133a5ce5
Cláudio Diniz <cfpdiniz@gmail.com>
2017-01-02 20:30:16 +0000

    Refactoring version generation with type class implicits, using implicitly and a companion object to get the predef implicits in place without impoting.

--- src/main/scala/com/byteslounge/slickrepo/meta/Version.scala
+++ src/main/scala/com/byteslounge/slickrepo/meta/Version.scala
@@ -1,7 +1,34 @@
 package com.byteslounge.slickrepo.meta
 
+import java.time.Instant
 
-abstract class VersionGenerator[T](){
+import com.byteslounge.slickrepo.datetime.DateTimeHelper
+
+
+trait VersionGenerator[T]{
   def init() : T
   def next(current : T): T
+}
+
+object VersionGenerator {
+
+  implicit object intVersionGenerator extends VersionGenerator[Int]{
+    override def init(): Int = 1
+    override def next(current: Int): Int = current + 1
+  }
+
+  implicit object longVersionGenerator extends  VersionGenerator[Long]{
+    override def init(): Long = 1
+    override def next(current: Long): Long = current + 1
+  }
+
+  implicit object instantVersionGenerator extends  VersionGenerator[Instant]{
+    override def init(): Instant = currentInstant()
+    override def next(current: Instant): Instant = currentInstant()
+  }
+
+  private def currentInstant(): Instant = {
+    DateTimeHelper.currentInstant
+  }
+
 }
--- src/main/scala/com/byteslounge/slickrepo/meta/VersionedEntity.scala
+++ src/main/scala/com/byteslounge/slickrepo/meta/VersionedEntity.scala
@@ -17,33 +17,9 @@ import java.time.Instant
 
 import com.byteslounge.slickrepo.datetime.DateTimeHelper
 
-abstract class VersionedEntity[T <: VersionedEntity[T, ID, V], ID, V](val version: Option[V] = None)(implicit versionGenerator : VersionGenerator[V]) extends Entity[T, ID] {
+abstract class VersionedEntity[T <: VersionedEntity[T, ID, V], ID, V : VersionGenerator](val version: Option[V] = None) extends Entity[T, ID] {
+  val generator = implicitly[VersionGenerator[V]]
   def withVersion(version: V): T
-  def withNewVersion(version: Option[V]): T = withVersion(version.fold(versionGenerator.init()){ v =>
-    versionGenerator.next(v)})
-}
-
-
-
-object VersionGeneratorImplicits {
-
-  implicit val intGenerator = new VersionGenerator[Int]{
-    override def init(): Int = 1
-    override def next(current: Int): Int = current + 1
-  }
-
-  implicit val longGenerator = new VersionGenerator[Long]{
-    override def init(): Long = 1
-    override def next(current: Long): Long = current + 1
-  }
-
-  implicit val instantGenerator = new VersionGenerator[Instant]{
-    override def init(): Instant = currentInstant()
-    override def next(current: Instant): Instant = currentInstant()
-  }
-
-   private def currentInstant(): Instant = {
-      DateTimeHelper.currentInstant
-   }
-
+  def withNewVersion(version: Option[V]): T = withVersion(version.fold(generator.init()){ v =>
+    generator.next(v)})
 }
