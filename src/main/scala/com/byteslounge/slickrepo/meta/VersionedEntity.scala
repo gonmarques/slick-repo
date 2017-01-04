980a3cf296f725ef4e67f43e7547b2bdacfa810d
Cláudio Diniz <cfpdiniz@gmail.com>
2017-01-04 13:13:15 +0000

    Change order of TestJodaTimeVersionedEntity companion object to match implicit evidence.

--- src/main/scala/com/byteslounge/slickrepo/meta/VersionedEntity.scala
+++ src/main/scala/com/byteslounge/slickrepo/meta/VersionedEntity.scala
@@ -13,10 +13,6 @@
 
 package com.byteslounge.slickrepo.meta
 
-import java.time.Instant
-
-import com.byteslounge.slickrepo.datetime.DateTimeHelper
-
 abstract class VersionedEntity[T <: VersionedEntity[T, ID, V], ID, V : VersionGenerator](val version: Option[V] = None) extends Entity[T, ID] {
   val generator = implicitly[VersionGenerator[V]]
   def withVersion(version: V): T
