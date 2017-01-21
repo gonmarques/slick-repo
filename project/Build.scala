54a0aab8e757ca1c17e4755c5e464332145f610b
Gonçalo Marques <goncalodinismarques@gmail.com>
2017-01-21 11:49:39 +0000

    Bump version

--- project/Build.scala
+++ project/Build.scala
@@ -52,7 +52,7 @@ object Build extends Build {
 
         name := "slick-repo",
         description := "CRUD Repositories for Slick based persistence Scala projects",
-        version := "1.2.5",
+        version := "1.2.6-SNAPSHOT",
 
         scalaVersion := "2.11.8",
         crossScalaVersions := Seq("2.11.8", "2.12.1", "2.10.6"),
